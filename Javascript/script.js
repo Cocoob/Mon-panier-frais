82019cca5b3ca75f9f1c5e2a175e2354d3c08a97
Corentin Bousquet <corentin.bousquet11@gmail.com>
2020-12-18 20:12:51 +0100

    Correction des bugs

--- Javascript/script.js
+++ Javascript/script.js
@@ -8,21 +8,32 @@ btn.addEventListener('click', function(){
     }
 })
 
-// Accordéon
-for ( i = 0 ; i < accordion.length ; i++ ) {
-    accordion[i].addEventListener('click', function(){
-        
-        
-                const active = document.querySelector('.active');
-        
-                if(active){
-                    active.classList.remove('active')
-                    this.classList.add('active')
-                }else{
-                    this.classList.add('active')
-                }     
-            })
-        }      
+
+
+    const accordionItemHeaders = document.querySelectorAll(".accordion .contentBox");
+
+    accordionItemHeaders.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener("click", event => {
+    
+    // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
+    
+     const currentlyActiveAccordionItemHeader = document.querySelector(".accordion .contentBox.active");
+    if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+       currentlyActiveAccordionItemHeader.classList.toggle("active");
+      currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = "auto";
+     }
+
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if(accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = "auto";
+    }
+    else {
+      accordionItemBody.style.maxHeight = "auto";
+    }
+    
+  });
+});
 
 
 
