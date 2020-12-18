c8ccb5d176855ac914c48c9ad1f967c66e0cb657
Corentin Bousquet <corentin.bousquet11@gmail.com>
2020-12-18 16:56:34 +0100

    Apparition/disparition menu accordéon

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,10 +1,32 @@
+const btn = document.getElementById('button');
 const accordion = document.getElementsByClassName('contentBox');
 
+// Bouton affiche tout les menus
+btn.addEventListener('click', function(){
+    
+    
+})
+
+// Accordéon
 for ( i = 0 ; i < accordion.length ; i++ ) {
     accordion[i].addEventListener('click', function(){
-        this.classList.toggle('active')
+        for ( i = 0 ; i < accordion.length ; i++ ) {
+            accordion[i].addEventListener('click', function(){
+        
+                const active = document.querySelector('.active');
+        
+                if(active){
+                    active.classList.remove('active')
+                    this.classList.add('active')
+                }else{
+                    this.classList.add('active')
+                }     
+            })
+        }
+        
     })
 }
 
 
 
+    
